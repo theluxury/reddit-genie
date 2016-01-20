cfc9527f3c6734fb0e5e158ec393b7aa0c68a366
root <root@ip-172-31-2-14.us-west-2.compute.internal>
2016-01-20 21:26:41 +0000

    Simple Kafka producer for tweets

--- kafka/TwitterConsumer.py
+++ kafka/TwitterConsumer.py
@@ -1,17 +1,35 @@
 import tweepy 
 import yaml
+import kafka
+import Constants
 
 config = yaml.safe_load(open("tweepy.yaml"))
 auth = tweepy.OAuthHandler(config['CONSUMER_KEY'], config['CONSUMER_SECRET'])
 auth.set_access_token(config['ACCESS_TOKEN'], config['ACCESS_SECRET'])
-
 api = tweepy.API(auth)
+ONE_HUNDRED_MOST_COMMON_WORDS = Constants.ONE_HUNDRED_MOST_COMMON_WORDS
+BATCH_SEND_EVERY_N = Constants.BATCH_SEND_EVERY_N
+BATCH_SEND_EVERY_T = Constants.BATCH_SEND_EVERY_T
+ERROR_LOG_FILENAME = Constants.ERROR_LOG_FILENAME
 
-class MyStreamListener(tweepy.StreamListener):
+class KafkaListener(tweepy.StreamListener):
+    def __init__(self, topic):
+        self.topic = topic
+        client = kafka.KafkaClient('localhost:9092')
+        self.producer = kafka.SimpleProducer(client, async=True, batch_send_every_n=BATCH_SEND_EVERY_N, batch_send_every_t=BATCH_SEND_EVERY_T)
+     
+    def on_data(self, data):
+        self.producer.send_messages(self.topic, data.encode('utf-8'))
 
-    def on_status(self, status):
-        print(status.text)
+    def on_error(self, error):
+        print "ERROR: " + error
+        if not os.path.isfile(ERROR_LOG_FILENAME):
+            f = file(ERROR_LOG_FILENAME, 'w')
+            f.close()
+        with open(logfilename, 'ab') as f:
+            f.write(datetime.now())
+            f.write(error)
 
-myStreamListener = MyStreamListener()
+myStreamListener = KafkaListener("my-topic")
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-myStream.filter(languages=['en'], track=['a', 'the'])
+myStream.filter(languages=['en'], track=ONE_HUNDRED_MOST_COMMON_WORDS)

fe22c33e1e72ee241dc3efe3f7b2c64668a07386
Ubuntu <ubuntu@ip-172-31-2-14.us-west-2.compute.internal>
2016-02-02 00:32:36 +0000

    Now puts to ES directly from S3 in spark job

--- spark/reddit_spark_job.py
+++ spark/reddit_spark_job.py
@@ -9,10 +9,40 @@ from cassandra.cluster import Cluster
 from boto.s3.connection import S3Connection
 from os import environ
 import json
+import logging
+from elasticsearch import Elasticsearch
 
 SPARK_ADDRESS = sys.argv[1]
 APP_NAME = "Reddit JSON Parser App"
 
+def put_to_elasticsearch(partition):
+    hosts=["ec2-52-35-132-98.us-west-2.compute.amazonaws.com", "ec2-52-34-176-185.us-west-2.compute.amazonaws.com", "ec2-52-89-115-101.us-west-2.compute.amazonaws.com", "ec2-52-88-254-51.us-west-2.compute.amazonaws.com", "ec2-52-88-247-22.us-west-2.compute.amazonaws.com", "ec2-52-89-166-197.us-west-2.compute.amazonaws.com"]
+    logs_file_path = os.path.dirname(os.path.abspath(__file__)) + '/logs/'
+    if not os.path.exists(logs_file_path):
+        os.makedirs(logs_file_path)
+    logging.basicConfig(filename=logs_file_path + 'reddit-spark-elasticsearch-bulk.log',level=logging.DEBUG)
+    es = Elasticsearch(
+        hosts,
+        port=9201,
+        sniff_on_start=True,    # sniff before doing anything
+        sniff_on_connection_fail=True,    # refresh nodes after a node fails to respond
+        sniffer_timeout=60, # and also every 60 seconds
+        timeout=30
+    )
+    i = 0
+    if partition:
+        json_string = ''
+        for item in partition:
+            json_string += item
+            json_string += '\n'
+#           preparedStmt = session.prepare("INSERT INTO {0} ({1}, {2}, {3}) VALUES (?, ?, ?)".format(GRAPH_NAME, PRIMARY_KEY, PARTITION_KEY, DICTIONARY))
+ #          boundStmt = preparedStmt.bind([item[0], item[1][0], item[1][1]]) #username then subreddit
+  #         session.execute(boundStmt)
+#        print json_string
+        es.bulk(body=json_string)
+    #except Exception as e:
+     #   logging.error("could not write to elasticsearch with error: {0}".format(str(e)))
+    
 def elastic_search_mapper(df):
     final_string = ""
     action_json = {'index': {'_index': 'reddit_filtered', '_type': 'comment', '_id': df.id}}  
@@ -33,7 +63,7 @@ def get_filtered_string(original_string):
     return ' '.join(filtered_tokenized_list)
     
 sc = SparkContext(SPARK_ADDRESS, "Reddit JSON Parser App")
-sqlContext = SQLContext(sc)
+sql_context = SQLContext(sc)
 
 conn = S3Connection(environ['AWS_ACCESS_KEY_ID'], environ['AWS_SECRET_ACCESS_KEY'])
 reddit_bucket = conn.get_bucket('reddit-comments')
@@ -42,18 +72,20 @@ for key in reddit_bucket.list():
     if '-' not in key.name.encode('utf-8'):
         continue
     logFile = 's3n://reddit-comments/' + key.name.encode('utf-8')
-    year = logFile.split('-')[1][-4:] 
+    year = logFile.split('-')[1][-4:]
+    if year != '2007':
+        continue
     month = logFile.split('-')[2]
     year_month = '{0}_{1}'.format(year, month)
-    if int(year) == 2007: # already did first year
-        continue
-    df = sqlContext.read.json(logFile)
+    df = sql_context.read.json(logFile)
     filtered_df = df.filter(df.author != '[deleted]')
-    final_string = filtered_df.map(lambda line: elastic_search_mapper(line))
-    outputDirectory = 's3n://mark-wang-test/reddit-es-comments-json/{0}/'.format(year_month)
+    final_rdd = filtered_df.map(lambda line: elastic_search_mapper(line))
+    final_rdd.foreachPartition(put_to_elasticsearch)
+
+#    outputDirectory = 's3n://mark-wang-test/reddit-es-comments-json/{0}/'.format(year_month)
     # delete files if it already exists.
-    for my_key in my_bucket.list(prefix='reddit-es-comments-json/{0}'.format(year_month)):
-        my_key.delete()
-    final_string.saveAsTextFile(outputDirectory)
+ #   for my_key in my_bucket.list(prefix='reddit-es-comments-json/{0}'.format(year_month)):
+ #       my_key.delete()
+#    final_string.saveAsTextFile(outputDirectory)
 
 sc.stop()

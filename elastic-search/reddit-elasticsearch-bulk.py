cb94cd14622797108a04803bc7476210a20255bb
Ubuntu <ubuntu@ip-172-31-2-14.us-west-2.compute.internal>
2016-02-05 21:32:34 +0000

    put ES bulk upload back in

--- elastic-search/reddit-elasticsearch-bulk.py
+++ elastic-search/reddit-elasticsearch-bulk.py
@@ -26,19 +26,19 @@ def download_files():
                 key.get_contents_to_filename(full_file_path)
     return tmp_dir
 
-# hosts=["ec2-52-35-132-98.us-west-2.compute.amazonaws.com", "ec2-52-34-176-185.us-west-2.compute.amazonaws.com", "ec2-52-89-115-101.us-west-2.compute.amazonaws.com", "ec2-52-88-254-51.us-west-2.compute.amazonaws.com", "ec2-52-88-247-22.us-west-2.compute.amazonaws.com", "ec2-52-89-166-197.us-west-2.compute.amazonaws.com"]
-# logsFilePath = os.path.dirname(os.path.abspath(__file__)) + '/logs/'
-# if not os.path.exists(logsFilePath):
-#     os.makedirs(logsFilePath)
-# logging.basicConfig(filename=logsFilePath + 'reddit-elasticsearch-bulk.log',level=logging.DEBUG)
-# es = Elasticsearch(
-#     hosts,
-#     port=9200,
-#     sniff_on_start=True,    # sniff before doing anything
-#     sniff_on_connection_fail=True,    # refresh nodes after a node fails to respond
-#     sniffer_timeout=60, # and also every 60 seconds
-#     timeout=30
-# )
+hosts=["ec2-52-35-132-98.us-west-2.compute.amazonaws.com", "ec2-52-34-176-185.us-west-2.compute.amazonaws.com", "ec2-52-89-115-101.us-west-2.compute.amazonaws.com", "ec2-52-88-254-51.us-west-2.compute.amazonaws.com", "ec2-52-88-247-22.us-west-2.compute.amazonaws.com", "ec2-52-89-166-197.us-west-2.compute.amazonaws.com"]
+logsFilePath = os.path.dirname(os.path.abspath(__file__)) + '/logs/'
+if not os.path.exists(logsFilePath):
+    os.makedirs(logsFilePath)
+logging.basicConfig(filename=logsFilePath + 'reddit-elasticsearch-bulk.log',level=logging.DEBUG)
+es = Elasticsearch(
+    hosts,
+    port=9200,
+    sniff_on_start=True,    # sniff before doing anything
+    sniff_on_connection_fail=True,    # refresh nodes after a node fails to respond
+    sniffer_timeout=60, # and also every 60 seconds
+    timeout=30
+)
 
 def main():
     tmp_dir = download_files()
@@ -48,7 +48,7 @@ def main():
         for filename in glob.glob(dirname[0] + '/*'):
             with open(filename) as file:
                 try:
-#                    es.bulk(body=file.read())
+                    es.bulk(body=file.read())
                     logging.debug("Sucessfully finished writing {0}".format(filename))
                     relative_path = '/'.join([filename.split('/')[-2], filename.split('/')[-1]])
                     print "uploaded json file {0}".format(relative_path)

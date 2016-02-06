a1517b0a930402d2522f8127dcea4bb306eecf03
Ubuntu <ubuntu@ip-172-31-2-10.us-west-2.compute.internal>
2016-02-06 03:00:20 +0000

    added comments to cassandra table

--- spark/reddit_to_cassandra.py
+++ spark/reddit_to_cassandra.py
@@ -35,7 +35,7 @@ def insert_into_cassandra(partition):
         prepared_stmt = session.prepare("INSERT INTO {0} ({1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(GRAPH_NAME, COLUMN_ONE, COLUMN_TWO, COLUMN_THREE, COLUMN_FOUR, COLUMN_FIVE, COLUMN_SIX, COLUMN_SEVEN, COLUMN_EIGHT, COLUMN_NINE, COLUMN_TEN))
         batch = BatchStatement(consistency_level=ConsistencyLevel.ONE)
         for item in partition:
-            bound_stmt = prepared_stmt.bind([item[0], item[1], long(item[2]), item[3], item[4], item[5], item[6], item[7], item[8], item[9]])
+            bound_stmt = prepared_stmt.bind([item[0], item[1], int(item[2]) * 1000, item[3], item[4], item[5], item[6], item[7], item[8], item[9]])
             session.execute(bound_stmt)
         session.shutdown()
         cluster.shutdown()
